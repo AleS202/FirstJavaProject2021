b95352ea869fd3ed5695cf40c0ebef03776aa559
AleS202 <93577521+AleS202@users.noreply.github.com>
2021-11-09 08:53:10 -0800

    Add files via upload
    Java Introduction Homework

---
+++ Main2.java
@@ -0,0 +1,26 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Alexandra");
+
+        short myFirstRandomNumber = 3;
+
+        short mySecondRandomNumber;
+        mySecondRandomNumber= 4;
+        System.out.println(myFirstRandomNumber+mySecondRandomNumber);
+
+        short oneRandomNumber = 16;
+        short anotherRandomNumber;
+        anotherRandomNumber = 8;
+        System.out.println(oneRandomNumber / 8 );
+
+        System.out.println(-5+(8*6));
+
+        System.out.println((55+9)%9);
+
+        System.out.println(20+(-3*5)/8);
+
+        System.out.println((5+15)/3*(2-8)%3);
+
+
+    }
+}
